09b5b3e2b930361b6489141300423025b456f44a
Javier Tarazona <elr490@usask.ca>
2024-12-21 16:47:07 -0600

    Form 0

--- frontend/src/components/LoadingIndicator.jsx
+++
@@ -1,9 +0,0 @@
-import "../styles/LoadingIndicator.css"
-
-const LoadingIndicator = () => {
-    return <div className="loader-container">
-        <div className="loader"></div>
-    </div>
-}
-
-export default LoadingIndicator
